b2954d3a52776f05e2f59fd009764d7b362f5233
ryannpierce <ryann@heysparkbox.com>
2013-07-26 10:06:33 -0400

    Add content-prototype-abridged

---
+++ content-prototype-abridged/featured-work.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<html>
+<head>
+  <title>Sparkbox Work</title>
+  <meta name="description" content="">
+  <link rel="stylesheet" href="content-style.css" media="screen">
+</head>
+
+<body>
+  <header>
+    <!--Main navigation would be included here.-->
+    <?php include("navigation.php") ?>
+  </header>
+
+  <section>
+    <article>
+    <h1>Featured Work</h1>
+    <a href="featured-work.php"><img src="" alt="Featured Image" /></a>
+    <h2><a href="featured-work.php">Stry.Us Long-Form News Website</a></h2>
+    <p><a href="http://stry.us">Go to stry.us</a></p>
+      <p>Working with Stry on a ground-up brand and website overhaul was one of those rare occasions where the client hands over near complete creative control. The direction from Stry founder, <a href="#">Dan Oshinsky</a>, was pretty much, "Make the most beautiful news website ever made." Stop by Stry.us for a look around. Be sure to set aside some time, however, because even if the beautiful, responsive design eventually loses your attention, the rich long-form articles will keep you there for hours. </p>
+   </article>
+   <h3>Tags: <a href="#">work</a>, <a href="#">design</a>, <a href="#">responsive</a>
+
+
+    <!--Footer featured some of our past clients' logos.-->
+    <?php include("footer.php") ?>
+  </body>
+  </html>
---
+++ content-prototype-abridged/navigation.php
@@ -0,0 +1,6 @@
+<nav>
+  <ol>
+    <li><a title="Sparkbox logo goes to home." href="index.php">Home</a></li>
+    <li><a title="Contact Page with form." href="contact-0.php">Contact</a></li>
+  </ol>
+</nav>
